5f1016bd6aa3f0bf3b4fa50b62f7eade29b1dace
Ducpacito <91423120+Ducpacito@users.noreply.github.com>
2022-01-24 16:47:07 +0700

    Update Tableau project.sql

--- Tableau project.sql
+++ Tableau project.sql
@@ -1,9 +1,8 @@
+Tableau dashboard: https://public.tableau.com/views/Onlineretailsales/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link
+
+link to the dataset: https://www.kaggle.com/tunguz/online-retail 
+
 -- Queries used for Tableau Project --
-
--- link to the dataset: https://www.kaggle.com/tunguz/online-retail 
-
--- Tableau dashboard: https://public.tableau.com/views/Onlineretailsales/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link
-
 
 --standardize InvoiceDate Format
 SELECT
